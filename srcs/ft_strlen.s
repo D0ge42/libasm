990e0fdb55afbb4a08b72280479a49c909d656a2
doge <lorenzo.nulli@txtgroup.com>
2025-06-20 17:11:43 +0200

    Managed to do ft_strlen and simple hello world in asm

---
+++ srcs/ft_strlen.s
@@ -0,0 +1,30 @@
+; We first copy pointer from rdi to rsi, since rdi is where the first argument will be stored.
+; We then xor the rcx register to set it at 0. This will be our counter. we could use any register
+; but rcx in 16/32-bit x86 was used as a loop counter.
+; we decleare a loop lable, and we start by comparing the current byte with a null byte
+; if condition is satisfied (jump if equal = je) then we jump to the done label.
+; else we keep going by incrementing pointer value by 1, and rcx (loop counter ) by 1 aswell and we loop again
+; once condition is satisfied (current byte is null byte) we jump to the done label.
+; in here we move the counter value (stored in rcx) into rdi. and we make the syscall
+; in this case we don't use syscall since we let the caller decide what he wants to do with the
+; result.
+; if we use a syscall the value of len would be stored as exit code
+
+global ft_strlen
+
+section .text
+ft_strlen:
+  mov rsi, rdi
+  xor rcx, rcx
+
+  loop_start:
+    cmp byte[rsi],0
+    je done
+    add rsi,1
+    inc rcx
+    jmp loop_start
+
+  done:
+    mov rax,rcx
+
+section .note.GNU-stack
--- srcs/hello_world.s
+++ srcs/hello_world.s
@@ -1,37 +1,18 @@
-; rax (Accumulator register) often used for return values and
-;; some arithmetic operations.
-; in 64-byt systems, rax is the 64 bit extension of the ax
-; register (which in turn is part of older eax,ax,al,ah reg hierarchy.)
+section .data
 
-;rdi (Destination index): often used to hold first arg
-; passed to a function
-
-;rsi (Source index) often used to hold second arg
-; passed to a function
-
-;rdx (Data register) often used to hold third argument
-; passed to a function.
-
-;in x86-64 System V calling convention (commonly used on Linux and maxOs)
-;the first six integer or pointer args to function are passed in registers:
-;rdi,rsi,rdx,rcx,r8,r9.
-; the return value is tipically stored in rax
-
-global _start
+  msg dw "Hello world", 0xA
+  msg_l equ $ - msg
 
 section .text
+  global _start
 
 _start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
+  mov rax, 1 ;write syscall
+  mov rdi, 1 ;stdout fd
+  mov rsi, msg ; message to be printed
+  mov rdx, msg_l ; message len
+  SYSCALL
 
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, world!", 10
-  msglen: equ $ - msg
+  mov rax, 60
+  xor rdi, rdi
+  SYSCALL

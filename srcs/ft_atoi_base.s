b9fd2baab0588e90739171d1c1b51e528a596373
doge <lorenzo.nulli@txtgroup.com>
2025-06-26 17:26:29 +0200

    Started working on base checker

--- srcs/ft_atoi_base.s
+++ srcs/ft_atoi_base.s
@@ -7,28 +7,86 @@ section .text
   default rel
   global ft_atoi_base
   extern printf
+  extern ft_strlen
 
 ft_atoi_base:
   push rbp ; +8
   mov rbp, rsp
 
+  mov r12, rsi ; save string in calle-saved register.
   mov rsi, rdi ; store str in 2nd arg of printf
-  mov r12, rdi ; save string in calle-saved register.
+  mov r13, rdi ; save string in calle-saved register.
 
   lea rdi, [fmt] ; put fmt value inside rdi
   call printf wrt ..plt ; call printf + 8 but -8 when ret
 
-  mov rsi, r12 ;
+  mov rsi, r13 ;
   call f_skip_wspaces ; this should return updated pointer to rsi
 
   lea rdi, [fmt]
 
   call printf wrt ..plt
 
+  call f_check_base
+
   mov rsp, rbp
   pop rbp
   ret
 
+
+  f_check_base:
+
+  ; check base_len
+  mov rdi, r12
+  call ft_strlen
+  cmp rax, 0x1
+  je l_base_len_err
+
+  xor al, al ; clean previously used al reg
+
+  l2:
+  ;loops that check for invalid chars in the base
+
+  ; check if end is reached
+  mov al, [r12]
+  test al, al
+  je l_ok
+
+  ; char comparison
+  cmp al, 0x2B
+  je l_base_err
+  cmp al, 0x2D
+  je l_base_err
+  cmp al, 0x20
+  je l_base_err
+
+  inc r12 ; inc r12
+  jmp l2 ; loop again
+
+  l_ok:
+    ret
+
+  l_base_err:
+  mov rax, 0x1
+  mov rdi, 0x2
+  lea rsi, ERR_HEXA
+  mov rdx, ERR_HEXA_LEN
+  syscall
+
+  mov rax, 60
+  mov rdi, 1
+  syscall
+
+  l_base_len_err:
+  mov rax, 0x1
+  mov rdi, 0x2
+  lea rsi, ERR_BTS
+  mov rdx, ERR_BTS_LEN
+  syscall
+
+  mov rax, 60
+  mov rdi, 1
+  syscall
 
   f_skip_wspaces:
   l1:
@@ -51,6 +109,12 @@ ft_atoi_base:
   inc rsi
   jmp l1
 
+section .data
+  ERR_HEXA db "Error: base can only contain hexadecimal values", 0xa
+  ERR_HEXA_LEN equ $- ERR_HEXA
+
+  ERR_BTS db "Error: base has to contain at least 2 hexadecimal values", 0xa
+  ERR_BTS_LEN equ $- ERR_BTS
 
 section .rodata
   fmt db "%s", 0xA
--- srcs/main2.c
+++ srcs/main2.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-extern void ft_atoi_base(const char *str);
-extern char *ft_strdup(const char *str);
-extern size_t ft_strlen(const char *str);
-extern char *ft_strcpy(char *dest, const char *src);
-extern int ft_strcmp(const char *s1, const char *s2);
-extern size_t ft_write(int fd, const void *buf, size_t count);
-extern size_t ft_read(int fd, const void *buf, size_t count);
+extern void ft_atoi_base(const char *str, char *base);
 
 int main(int ac, char **av)
 {
-  ft_atoi_base(av[1]);
+  ft_atoi_base(av[1],"0A");
   return 0;
 }
 

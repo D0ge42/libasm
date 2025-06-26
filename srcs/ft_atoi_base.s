da05dd08f33faeb7cbe6a7df6d5478f708173911
doge <lorenzo.nulli@txtgroup.com>
2025-06-26 15:50:14 +0200

    Modified function to skip white spaces

--- srcs/ft_atoi_base.s
+++ srcs/ft_atoi_base.s
@@ -1,28 +1,29 @@
-default rel
-global ft_atoi_base
-extern printf
+; when we call a non-internal function the stack must be 16 bytes aligned.
+; since at the start we push rbp, stack is now 8 bytes misaligned.
+; but then we call printf and "call" pushes the return address on the stack, making it aligned for a
+; function call.
 
-section .rodata
-  fmt db "%p", 0xA
-
+section .text
+  default rel
+  global ft_atoi_base
+  extern printf
 
 ft_atoi_base:
-  push rbp
+  push rbp ; +8
   mov rbp, rsp
 
-  sub rsp, 8
+  mov rsi, rdi ; store str in 2nd arg of printf
+  mov r12, rdi ; save string in calle-saved register.
+
+  lea rdi, [fmt] ; put fmt value inside rdi
+  call printf wrt ..plt ; call printf + 8 but -8 when ret
+
+  mov rsi, r12 ;
+  call f_skip_wspaces ; this should return updated pointer to rsi
+
+  lea rdi, [fmt]
+
   call printf wrt ..plt
-  add rsp, 8
-
-  call f_skip_wspaces
-
-  sub rsp, 8
-
-  mov rsi, rdi
-  lea rdx,fmt
-  call printf wrt ..plt
-
-  add rsp, 8
 
   mov rsp, rbp
   pop rbp
@@ -31,7 +32,7 @@ ft_atoi_base:
 
   f_skip_wspaces:
   l1:
-  mov al, [rdi]
+  mov al, [rsi]
   cmp al, 0x9
   je l_inc
   cmp al, 0xA
@@ -47,8 +48,11 @@ ft_atoi_base:
   ret
 
   l_inc:
-  inc rdi
+  inc rsi
   jmp l1
 
+
+section .rodata
+  fmt db "%s", 0xA
 
 section .note.GNU-stack
--- srcs/main2.c
+++ srcs/main2.c
@@ -1,10 +1,16 @@
-
 #include <stdio.h>
-void ft_atoi_base(const char *str);
-void increase_ptr(char **av);
+
+extern void ft_atoi_base(const char *str);
+extern char *ft_strdup(const char *str);
+extern size_t ft_strlen(const char *str);
+extern char *ft_strcpy(char *dest, const char *src);
+extern int ft_strcmp(const char *s1, const char *s2);
+extern size_t ft_write(int fd, const void *buf, size_t count);
+extern size_t ft_read(int fd, const void *buf, size_t count);
 
 int main(int ac, char **av)
 {
   ft_atoi_base(av[1]);
+  return 0;
 }
 
